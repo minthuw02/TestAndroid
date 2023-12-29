df72f86605540b184149a851bb69b0a7bfda8585
minthuw02 <nguyenthu1328@gmail.com>
2023-12-29 15:28:35 +0700

    2

--- app/src/main/java/com/example/test/DbHelper.java
+++ app/src/main/java/com/example/test/DbHelper.java
@@ -12,10 +12,10 @@ public class DbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase){
         String sql = "CREATE TABLE SANPHAM(ID INTEGER PRIMARY KEY AUTOINCREMENT," + "IMAGE TEXT, NAME TEXT, PRICE INTEGER)";
         sqLiteDatabase.execSQL(sql);
-        String data = "INSERT INTO TODO VALUES(1,https://www.google.com.vn/url?sa=i&url=https%3A%2F%2Fwww.hoteljob.vn%2Ftin-tuc%2F8-bi-quyet-chup-anh-mon-an-ngon-mat-bang-dien-thoai-quang-ba-cho-nha-hang&psig=AOvVaw1JMm06L4xVt9rOl04l0arl&ust=1703921588607000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPju08eRtIMDFQAAAAAdAAAAABAD,'Veggie tomato mix',1999),"+
-                "(2,https://www.google.com.vn/url?sa=i&url=https%3A%2F%2Fvn.lovepik.com%2Fimage-500931330%2Fcool-cold-dish.html&psig=AOvVaw1JMm06L4xVt9rOl04l0arl&ust=1703921588607000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPju08eRtIMDFQAAAAAdAAAAABAI,'Fishwith mix orange',1999),"+
-                "(3,https://www.google.com.vn/url?sa=i&url=https%3A%2F%2Fprinceproduction.vn%2Falbum-category%2Fchup-anh-mon-an-nha-hang%2F&psig=AOvVaw1JMm06L4xVt9rOl04l0arl&ust=1703921588607000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPju08eRtIMDFQAAAAAdAAAAABAQ,'Salad',1990),"+
-                "(4,https://www.google.com.vn/url?sa=i&url=https%3A%2F%2Fpxhere.com%2Fvi%2Fphoto%2F1445049&psig=AOvVaw1JMm06L4xVt9rOl04l0arl&ust=1703921588607000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPju08eRtIMDFQAAAAAdAAAAABAY,'Beefsteaf',2500)";
+        String data = "INSERT INTO TODO VALUES(1,drawable/images4.jpg,'Veggie tomato mix',1999),"+
+                "(2,drawable/images.jpg,'Fishwith mix orange',1999),"+
+                "(3,drawable/download.jpg,'Salad',1990),"+
+                "(4,drawable/images5.jpg,'Beefsteaf',2500)";
         sqLiteDatabase.execSQL(data);
     }
     @Override
